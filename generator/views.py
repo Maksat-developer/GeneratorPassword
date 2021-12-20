9a3dc5c958456ae0a2851ee57a1d2598a205999e
Stalker <gangsta.okulus02@gmail.com>
2021-12-20 09:13:10 +0600

    generator

---
+++ generator/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+def Home(request):
+    return render (request, 'generator/home.html')
+
+
+def password(request):
+
+    Characters = list('qwertyuiopasdfghjklzxcvbnm')
+
+    if request.GET.get('uppercase'):
+        Characters.extend(list('QWERTYUIOPASDFGHJKLZXCVBNM'))
+
+    if request.GET.get('special'):
+        Characters.extend(list('!@#$%^&*()_+}|:"?>><?,./;\|/.'))
+
+    if request.GET.get('number'):
+        Characters.extend(list('1234567890'))
+
+    lenght = int(request.GET.get('lenght', 10))
+
+    Thepassword = ''
+
+    for x in range(lenght):
+        Thepassword += random.choice(Characters)
+
+    return render (request, 'generator/password.html', {'password': Thepassword})
